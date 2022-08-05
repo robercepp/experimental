3083c11c558e1a75c6f3d5f9433a6f4d6faa3d93
Robertino Cepparo <robercepp@hotmail.com>
2022-08-05 00:03:37 -0300

    cuarto push

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@ catalogoDeBusqueda.push(libretaChicasGamer, libretaSixFanarts, libretaLuluMartin
 //variables y constantes generales
 let nombre = "";
 let apellido = "";
+let email = ""
 const iva = 1.21;
 let titulo = document.createElement("h4");
 document.getElementsByClassName("titulo-relativo")[0].appendChild(titulo);
@@ -117,16 +118,20 @@ function BIENVENIDA() {
         } else {
             const chequearUsuario = obj => obj.nombreDeUsuario === e.target[0].value;
             if (usuarios.some(chequearUsuario) === false) {
-                alerta.innerHTML = "No se encuentra al usuario"
+                alerta.innerHTML = "el nombre de usuario no se encuentra o la contraseña es incorrecta."
             } else {
                 const encontrado = usuarios.find((user) => {
                     return user.nombreDeUsuario === e.target[0].value;
                 });
                 if (encontrado.contrasena === e.target[1].value) {
-                    nombre = encontrado.nombre
+                    nombre = encontrado.nombre;
+                    apellido = encontrado.apellido;
+                    email = encontrado.email;
                     bienvenido.innerHTML = "Bienvenido/a, " + nombre;
                     alerta.replaceChildren();
                     MENUPRINCIPAL(), 0;
+                } else {
+                    alerta.innerHTML = "el nombre de usuario no se encuentra o la contraseña es incorrecta."
                 }
             }
         }
@@ -303,41 +308,50 @@ function CREARUSUARIO() {
             var ageDate = new Date(edadDifMs);
             return Math.abs(ageDate.getUTCFullYear() - años);
         }
-        if (e.target[6].value === "" || e.target[7].value === "") {
+
+        if (e.target[0].value === "") {
+            alerta.innerHTML = "debes escribir un nombre de usuario";
+            document.querySelector("#nombre-usuario").classList.toggle("red-border");
+        } else if (usuarios.some(chequearUsuario) === true || usuarios == []) {
+            alerta.innerHTML = "Lo sentimos, el nombre de usuario ya está ocupado, por favor elija otro.";
+            document.querySelector("#nombre-usuario").classList.toggle("red-border")
+        } else if (e.target[1].value === "") {
+            alerta.innerHTML = "debes escribir tu nombre";
+            document.querySelector("#nombre").classList.toggle("red-border");
+        } else if (e.target[2].value === "") {
+            alerta.innerHTML = "debes escribir un apellido";
+            document.querySelector("#apellido").classList.toggle("red-border");
+        } else if (e.target[3].value === "") {
+            alerta.innerHTML = "debes escribir tu fecha de nacimiento";
+            document.querySelector("#fecha-nac").classList.toggle("red-border");
+        } else if (e.target[4].value === "" || e.target[5].value === "") {
+            alerta.innerHTML = "cuidado, debes escribir una email válido.";
+            document.querySelector("#email-1").classList.toggle("red-border");
+            document.querySelector("#email-2").classList.toggle("red-border");
+        } else if (e.target[4].value !== e.target[5].value) {
+            alerta.innerHTML = "los e-mails escritos no coinciden. por favor intente nuevamente.";
+            document.querySelector("#email-1").classList.toggle("red-border");
+            document.querySelector("#email-2").classList.toggle("red-border");
+        } else if (e.target[6].value === "" || e.target[7].value === "") {
             alerta.innerHTML = "cuidado, debes escribir una contraseña ";
             document.querySelector("#contraseña-1").classList.toggle("red-border");
             document.querySelector("#contraseña-2").classList.toggle("red-border");
+        } else if (e.target[6].value !== e.target[7].value) {
+            alerta.innerHTML = "las contraseñas no coinciden, intentelo nuevamente.";
+            document.querySelector("#contraseña-1").classList.toggle("red-border");
+            document.querySelector("#contraseña-2").classList.toggle("red-border");
+        } else if (e.target[8].value === "" || e.target[9].value === "") {
+            alerta.innerHTML = "cuidado, debes escribir una pregunta y una respuesta de seguridad.";
+            document.querySelector("#pregunta").classList.toggle("red-border");
+            document.querySelector("#respuesta").classList.toggle("red-border");
         } else {
-            if (e.target[4].value === "" || e.target[5].value === "") {
-                alerta.innerHTML = "cuidado, debes escribir una email válido.";
-                document.querySelector("#email-1").classList.toggle("red-border");
-                document.querySelector("#email-2").classList.toggle("red-border");
-            } else {
-                if (e.target[6].value === e.target[7].value) {
-                    if (e.target[4].value === e.target[5].value) {
-                        if (usuarios.some(chequearUsuario) === false || usuarios == []) {
-                            idcounter = idcounter + 1
-                            const newUser = new Usuario(idcounter, e.target[0].value, e.target[1].value, e.target[2].value, calcularEdad(años), e.target[4].value, e.target[6].value, e.target[8].value, e.target[9].value);
-                            usuarios.push(newUser)
-                            document.querySelectorAll('#formulario input').forEach((input) => {
-                                input.value = '';
-                            });
-                            alerta.innerHTML = "Usuario creado satisfactoriamente";
-                        } else {
-                            alerta.innerHTML = "Lo sentimos, el nombre de usuario ya está ocupado, por favor elija otro.";
-                            document.querySelector("#nombre-usuario").classList.toggle("red-border")
-                        }
-                    } else {
-                        alerta.innerHTML = "los e-mails escritos no coinciden. por favor intente nuevamente.";
-                        document.querySelector("#email-1").classList.toggle("red-border");
-                        document.querySelector("#email-2").classList.toggle("red-border");
-                    }
-                } else {
-                    alerta.innerHTML = "las contraseñas no coinciden, intentelo nuevamente.";
-                    document.querySelector("#contraseña-1").classList.toggle("red-border");
-                    document.querySelector("#contraseña-2").classList.toggle("red-border");
-                }
-            }
+            idcounter = idcounter + 1
+            const newUser = new Usuario(idcounter, e.target[0].value, e.target[1].value, e.target[2].value, calcularEdad(años), e.target[4].value, e.target[6].value, e.target[8].value, e.target[9].value);
+            usuarios.push(newUser)
+            document.querySelectorAll('#formulario input').forEach((input) => {
+                input.value = '';
+            });
+            alerta.innerHTML = "Usuario creado satisfactoriamente";
         }
     });
 }
@@ -628,24 +642,38 @@ function MENUCARRITO() {
         resultado.innerHTML = `
                     <div class="td-tipo">${carritoDeCompras.tipo}</div>
                                 <div class="td-nombre">${carritoDeCompras.nombre}</div>
-                                <div class="td-precio">$${carritoDeCompras.precio}</div>
+                                <div class="td-precio">$${(carritoDeCompras.precio*iva).toFixed(2)}</div>
                                 <div class="td-cantidad">${carritoDeCompras.cantidad}</div>
-                                <div class="td-subtotal">$${carritoDeCompras.subtotal}</div>
-                                <button id="agregado-${numeracion}" class="boton-contextual">quitar</button>
+                                <div class="td-subtotal">$${(carritoDeCompras.subtotal*iva).toFixed(2)}</div>
+                                <div class="td-form"> <form id="quitar-${numeracion}">
+                                <input style='width:2em' type="number" min="1">
+                                <button class="boton-contextual">quitar</button>
+                                </form>
                                 `;
         eliminar(numeracion);
     }, carritoDeCompras);
 
     function eliminar(numero) {
-        const quitar = document.querySelector("#agregado-" + numero);
-        quitar.addEventListener("mouseup", (e) => {
+        const quitar = document.querySelector("#quitar-" + numero);
+        quitar.addEventListener("submit", (e) => {
             e.preventDefault();
-            catalogoDeBusqueda[carritoDeCompras[(numero - 1)].id].stock = catalogoDeBusqueda[carritoDeCompras[(numero - 1)].id].stock + carritoDeCompras[(numero - 1)].cantidad;
-            alerta.innerHTML = carritoDeCompras[(numero - 1)].cantidad + "x " + carritoDeCompras[(numero - 1)].nombre + " ha sido eliminado del carrito de compras.";
-            const borrar = document.querySelector("#item-" + numero);
-            borrar.remove();
-            let borrado = carritoDeCompras.splice(numero - 1, 1);
-            MENUCARRITO();
+            cantidades = parseInt(e.target[0].value);
+            if (carritoDeCompras[(numero - 1)].cantidad >= cantidades) {
+                carritoDeCompras[(numero - 1)].cantidad = carritoDeCompras[(numero - 1)].cantidad - cantidades;
+                carritoDeCompras[(numero - 1)].subtotal = carritoDeCompras[(numero - 1)].subtotal - (carritoDeCompras[(numero - 1)].precio * cantidades);
+                catalogoDeBusqueda[carritoDeCompras[(numero - 1)].id].stock = catalogoDeBusqueda[carritoDeCompras[(numero - 1)].id].stock + cantidades;
+                alerta.innerHTML = cantidades + "x " + carritoDeCompras[(numero - 1)].nombre + " ha sido eliminado del carrito de compras.";
+                if (carritoDeCompras[(numero - 1)].cantidad === 0) {
+                    const borrar = document.querySelector("#item-" + numero);
+                    borrar.remove();
+                    let borrado = carritoDeCompras.splice(numero - 1, 1);
+                }
+                MENUCARRITO();
+            } else if (e.target[0].value === "") {
+                alerta.innerHTML = "debes ingresar una cantidad para quitar";
+            } else {
+                alerta.innerHTML = "cuidado, no puedes quitar mas items de los que has agregado";
+            }
         });
     }
     salida.className = "foot"
@@ -705,11 +733,11 @@ function MENUCARRITO() {
                 });
                 if (pago == precioConIva.toFixed(2)) {
                     alerta.innerHTML = "El pago de: $" + precioConIva.toFixed(2) + " se ha acreditado correctamente.";
-                    CIERREDECOMPRA();
+                    ENTREGABOLETA();
                 } else if (pago > precioConIva) {
                     let vuelto = pago - precioConIva.toFixed(2);
                     alerta.innerHTML = "Al parecer nos has enviado mas dinero del que era necesario, por ello te reenviamos $" + vuelto.toFixed(2) + " como vuelto por tu compra.";
-                    CIERREDECOMPRA();
+                    ENTREGABOLETA();
                 } else if (pago < precioConIva && pago > 0) {
                     let pagoInsuficiente = precioConIva.toFixed(2) - pago;
                     alerta.innerHTML = "Vaya!, al parecer has pagado $" + pago + ". Lamentablemente te faltan $" + pagoInsuficiente.toFixed(2) + " para completar los $" + precioConIva.toFixed(2) + " que se necesitan.</ br>Te devolvemos el dinero, Volvamos a completar la transacción.";
@@ -719,40 +747,18 @@ function MENUCARRITO() {
     });
 }
 
-function CIERREDECOMPRA() {
-    const listadoCarrito = carritoDeCompras.map((carritoDeCompras) => "-" + carritoDeCompras.cantidad + "x " + carritoDeCompras.nombre + "<br />Subtotal: $" + carritoDeCompras.subtotal + "<br />");
+function ENTREGABOLETA() {
+    const listadoCarrito = carritoDeCompras.map((carritoDeCompras) => "-" + carritoDeCompras.cantidad + "x " + carritoDeCompras.nombre + "<br />Subtotal: $" + carritoDeCompras.subtotal * iva + "<br />");
     const precioSinIva = carritoDeCompras.reduce((acumulador, carritoDeCompras) => {
         return acumulador + carritoDeCompras.subtotal;
     }, 0);
     const precioConIva = precioSinIva * iva;
-    contenido.innerHTML = `<p>Por favor introduzca a continuación un correo electrónico para poder enviarte los productos.</p>
-    <form id="formulario">
-    <div class=buscador-container>
-    <input id="buscador" placeholder="usuario@servidor.com" style='width:20em' type="email"></input>
-    <button class="boton-buscar" type="submit">enviar</button>
-    </div>
-    </form>
-    <button id="salida">volver</button>
-    `
-    const formulario = document.getElementById("formulario");
-    formulario.addEventListener("submit", (e) => {
-        e.preventDefault();
-        email = e.target[0].value;
-        if (email === "") {
-            alerta.innerHTML = "Por favor llene los campos antes de continuar"
-        } else {
-            ENTREGABOLETA();
-        };
-    })
-
-    function ENTREGABOLETA() {
-        const fechaDeCompra = new Date();
-        const dia = fechaDeCompra.toLocaleDateString();
-        const hora = fechaDeCompra.toLocaleTimeString();
-        contenido.innerHTML = "Su boleta de compra:<br />Factura tipo C consumidor final<br />Anabella Avena n°0001-000001<br />Fecha de compra: " + dia + "<br />hora: " + hora + "<br />Nombre: " + nombre + "<br />Apellido: " + apellido + "<br />email: " + email + "<br />Items comprados:<br />" + listadoCarrito + "<br />Total monto: $" + precioConIva.toFixed(2) + "-.<br />Muchas gracias por su compra!";
-        carritoDeCompras.splice(0, carritoDeCompras.length);
-        alerta.innerHTML = "Ha salido de la tienda, esperamos volver a verlo pronto."
-    }
+    const fechaDeCompra = new Date();
+    const dia = fechaDeCompra.toLocaleDateString();
+    const hora = fechaDeCompra.toLocaleTimeString();
+    contenido.innerHTML = "Su boleta de compra:<br />Factura tipo C consumidor final<br />Anabella Avena n°0001-000001<br />Fecha de compra: " + dia + "<br />hora: " + hora + "<br />Nombre: " + nombre + "<br />Apellido: " + apellido + "<br />email: " + email + "<br />Items comprados:<br />" + listadoCarrito + "<br />Total monto: $" + precioConIva.toFixed(2) + "-.<br />Muchas gracias por su compra!";
+    carritoDeCompras.splice(0, carritoDeCompras.length);
+    alerta.innerHTML += "</ br> Ha salido de la tienda, esperamos volver a verlo pronto."
 }
 
 function EXITPROGRAM() {
